33c7ee8c360f8b4be156cb97c10b87cbf1d2910c
Nahidahmmed <nahidahmmed411@gmail.com>
2024-03-05 01:15:26 +0600

    mobile divice navbar is added

--- src/Components/Welcome/Welcome.jsx
+++ src/Components/Welcome/Welcome.jsx
@@ -2,7 +2,14 @@ import image from "../../assets/logo/image2.jpg";
 
 export default function Welcome() {
   return (
-    <div className="lg:flex md:flex w-full lg:px-[10%] pt-[15%] h-screen">
+    <div className="lg:flex-row-reverse md:flex-row-reverse md:flex w-full lg:px-[10%] md:pt-[15%] pt-[45%] h-screen">
+        <div className="w-full lg:w-[50%] md:w-[50%] relative">
+        <img
+          className="w-[300px] rounded-xl mx-auto md:mt-5 border-4 border-gray-200 hover:border-gradient-500 hover:scale-105 transform transition duration-300 ease-in-out"
+          src={image}
+          alt=""
+        />
+      </div>
       <div className="lg:w-[50%] md:w-[50%] lg:pl-28 md:pl-10 mx-5">
         <p className="text-2xl font-mono">Welcome! I'm</p>
         <h1 className="text-7xl font-bold text-[#344955] mt-5">Nahid</h1>
@@ -17,9 +24,7 @@ export default function Welcome() {
         </div>
         <div>social links in Icons</div>
       </div>
-      <div className='w-full lg:w-[50%] relative'>
-    <img className='w-[300px] rounded-xl mx-auto md:mt-5 border-4 border-gray-200 hover:border-gradient-500 hover:scale-105 transform transition duration-300 ease-in-out' src={image} alt="" />
-</div>
+      
     </div>
   );
 }
